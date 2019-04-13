f907c7064d89e8216af053582353a15e32e63655
Scallop Ye <16952958+yescallop@users.noreply.github.com>
2019-04-13 23:58:14 +0800

    Add CSV structure, directly write data into files

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 /**
@@ -25,18 +31,18 @@ public class Main {
                 return;
             }
             map.entrySet().stream()
-                    .filter(e -> !each.containsKey(e.getKey()) && !e.getValue().ended)
+                    .filter(e -> !each.containsKey(e.getKey()) && !e.getValue().deprecated)
                     .forEach(e -> {
                         Area a = e.getValue();
                         a.time.add(year);
-                        a.ended = true;
+                        a.deprecated = true;
                         a.names.add("-");
                     });
             each.forEach((k, v) -> {
                 if (map.containsKey(k)) {
                     Area a = map.get(k);
-                    if (a.ended)
-                        a.ended = false;
+                    if (a.deprecated)
+                        a.deprecated = false;
                     if (!a.names.get(a.names.size() - 1).equals(v)) {
                         a.names.add(v);
                         a.time.add(year);
@@ -55,22 +61,106 @@ public class Main {
             e.printStackTrace();
         }
 
-        int[] codes = map.keySet().stream().mapToInt(Integer::intValue).sorted().toArray();
-        for (int c : codes) {
-            Area a = map.get(c);
-            System.out.printf("%d\t%s\t%s\n", c, a.names, a.time);
+        try {
+            BufferedWriter bwCsv = Files.newBufferedWriter(
+                    Paths.get("result.csv"),
+                    StandardCharsets.UTF_8,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING
+            );
+            bwCsv.write('\ufeff'); //BOM
+            bwCsv.write("代码,名称,级别,状态,变更时间,是否自治,是否市辖区,是否县级市,一级行政区,二级行政区,历史名称,历史变更时间,更多\n");
+
+            BufferedWriter bwTxt = Files.newBufferedWriter(
+                    Paths.get("result.txt"),
+                    StandardCharsets.UTF_8,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING
+            );
+
+            int[] codes = map.keySet().stream().mapToInt(Integer::intValue).sorted().toArray();
+            for (int c : codes) {
+                Area a = map.get(c);
+                String name = a.lastName();
+                boolean isAutonomous = !a.deprecated && name.contains("自治");
+                Level level = Level.fromCode(c);
+                boolean isCountyLevelCity = !a.deprecated && level == Level.COUNTY && name.endsWith("市");
+                boolean isDistrict = !a.deprecated && level == Level.COUNTY && name.endsWith("区");
+                String primaryDistrict = map.get(c / 10000 * 10000).lastName();
+                String secondaryDistrict = "";
+
+                if (level != Level.PROVINCE && !a.deprecated) {
+                    Area cda = map.get(c / 100 * 100);
+                    if (cda == null || cda.deprecated) {
+                        secondaryDistrict = "直管";
+                    } else {
+                        secondaryDistrict = cda.lastName();
+                    }
+                }
+
+                bwCsv.write(String.format("%d,%s,%s,%s,%d,%s,%s,%s,%s,%s",
+                        c, name, level.description, a.deprecated ? "弃用" : "使用", a.lastTime(),
+                        isAutonomous ? "是" : "否", isDistrict ? "是" : "否", isCountyLevelCity ? "是" : "否",
+                        primaryDistrict, secondaryDistrict
+                ));
+
+                int size = a.names.size();
+                if (size != 1) {
+                    for (int i = size - 2; i >= 0; i--) {
+                        bwCsv.write(',' + a.names.get(i) + ',' + a.time.get(i));
+                    }
+                }
+                bwCsv.write('\n');
+                bwCsv.flush();
+
+                bwTxt.write(String.format("%d\t%s\t%s\n", c, a.names, a.time));
+                bwTxt.flush();
+            }
+            bwCsv.close();
+            bwTxt.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private enum Level {
+        PROVINCE("省级"),
+        PREFECTURE("地级"),
+        COUNTY("县级");
+
+        String description;
+
+        Level(String description) {
+            this.description = description;
+        }
+
+        public static Level fromCode(int code) {
+            if ((code % 100) != 0)
+                return COUNTY;
+            code /= 100;
+            if ((code % 100) != 0)
+                return PREFECTURE;
+            return PROVINCE;
         }
     }
 
     private static class Area {
-
-        List<String> names = new LinkedList<>();
-        Set<Integer> time = new LinkedHashSet<>();
-        boolean ended = false;
+        List<String> names = new ArrayList<>();
+        List<Integer> time = new ArrayList<>();
+        boolean deprecated = false;
 
         Area(String name, int start) {
             this.names.add(name);
             this.time.add(start);
+        }
+
+        public String lastName() {
+            return names.get(names.size() - 1);
+        }
+
+        public int lastTime() {
+            return time.get(time.size() - 1);
         }
     }
 }
