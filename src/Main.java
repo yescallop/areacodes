0964564ea7b37e18f264f340b9db3fd80ae9a3fd
Scallop Ye <16952958+yescallop@users.noreply.github.com>
2020-01-05 11:56:23 +0800

    Improve texts for status

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,11 @@ import java.util.function.Consumer;
  * @author Scallop Ye
  */
 public class Main {
+
+    public static final int FLAG_NONE = 0;
+    public static final int FLAG_TEMPORARY_DEPRECATION = 1;
+    public static final int FLAG_PERMANENT_DEPRECATION = 2;
+
     public static void main(String[] args) {
         Map<Integer, Area> map = new HashMap<>();
         Map<Integer, String> each = new HashMap<>();
@@ -83,21 +88,31 @@ public class Main {
                 Area a = map.get(c);
 
                 int size = a.names.size();
+                int flag = FLAG_NONE;
                 if (size == 1) {
-                    writeArea(bwCsv, map, c, a.names.get(0), a.time.get(0), null, true);
+                    writeArea(bwCsv, map, c, a.names.get(0), a.time.get(0), null, flag);
                 } else if (a.deprecated) {
                     for (int i = size - 2; i >= 0; i--) {
                         String name = a.names.get(i);
-                        if (!name.equals("-"))
-                            writeArea(bwCsv, map, c, name, a.time.get(i), a.time.get(i + 1), i == size - 2);
+                        if (name.equals("-")) {
+                            flag = FLAG_TEMPORARY_DEPRECATION;
+                        } else {
+                            writeArea(bwCsv, map, c, name, a.time.get(i), a.time.get(i + 1),
+                                    i == size - 2 ? FLAG_PERMANENT_DEPRECATION : flag);
+                            flag = FLAG_NONE;
+                        }
                     }
                 } else {
                     for (int i = size - 1; i >= 0; i--) {
                         String name = a.names.get(i);
                         if (i == size - 1)
-                            writeArea(bwCsv, map, c, name, a.time.get(i), null, true);
-                        else if (!name.equals("-"))
-                            writeArea(bwCsv, map, c, name, a.time.get(i), a.time.get(i + 1), i == size - 1);
+                            writeArea(bwCsv, map, c, name, a.time.get(i), null, flag);
+                        else if (name.equals("-")) {
+                            flag = FLAG_TEMPORARY_DEPRECATION;
+                        } else {
+                            writeArea(bwCsv, map, c, name, a.time.get(i), a.time.get(i + 1), flag);
+                            flag = FLAG_NONE;
+                        }
                     }
                 }
 
@@ -113,7 +128,7 @@ public class Main {
     }
 
     private static void writeArea(BufferedWriter bw, Map<Integer, Area> map,
-                                  int code, String name, int startTime, Integer endTime, boolean last) throws IOException {
+                                  int code, String name, int startTime, Integer endTime, int flag) throws IOException {
         Level level = Level.fromCode(code);
 
         boolean isAutonomous = name.contains("自治");
@@ -141,7 +156,8 @@ public class Main {
         //代码,一级行政区,二级行政区（弃用前）,名称,级别,状态,启用时间,弃用时间,是否自治,是否市辖区,是否县级市
         bw.write(String.format("%d,%s,%s,%s,%s,%s,%d,%s,%s,%s,%s\n",
                 code, primaryDistrict, secondaryDistrict, name, level.description,
-                endTime == null ? "启用" : (last ? "代码弃用" : "名称弃用"),
+                endTime == null ? "启用" :
+                        (flag == FLAG_NONE ? "更名" : (flag == FLAG_TEMPORARY_DEPRECATION ? "暂弃" : "弃用")),
                 startTime, endTime == null ? "" : endTime,
                 isAutonomous ? "是" : "否", isDistrict ? "是" : "否", isCountyLevelCity ? "是" : "否"
         ));
