7b6881636ab29183595b72fc1b79d7e67398493c
yescallop <yescallop@gmail.com>
2021-12-03 20:41:48 +0800

    Generate diffs

--- src/bin/gen_diff.rs
+++ src/bin/gen_diff.rs
@@ -1,18 +1,37 @@
-use std::{fs, process::Command};
+use std::{
+    fs::{self, File},
+    io::{BufWriter, Result, Write},
+    process::Command,
+};
 
-fn main() {
+fn main() -> Result<()> {
     let file_stems = file_name_iter().collect::<Vec<_>>();
     for pair in file_stems.windows(2) {
-        Command::new("git")
+        let res = Command::new("git")
             .args(["diff", "-U0", "--no-index"])
             .args([
-                format!("--output=diff/{}-{}.diff", pair[0], pair[1]),
                 format!("data/{}.txt", pair[0]),
                 format!("data/{}.txt", pair[1]),
             ])
-            .spawn()
-            .unwrap();
+            .output()?
+            .stdout;
+
+        if res.is_empty() {
+            continue;
+        }
+        let res = unsafe { String::from_utf8_unchecked(res) };
+
+        let file = File::create(format!("diff/{}-{}.diff", pair[0], pair[1]))?;
+        let mut bw = BufWriter::new(file);
+
+        for line in res.lines().skip(4) {
+            if !line.starts_with(['@', '\\'].as_ref()) {
+                writeln!(bw, "{}", line)?;
+            }
+        }
+        bw.flush()?;
     }
+    Ok(())
 }
 
 fn file_name_iter() -> impl Iterator<Item = String> {
