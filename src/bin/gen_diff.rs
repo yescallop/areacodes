a5615fb49166964ef9f55afe9c9a5e84522fa148
yescallop <yescallop@gmail.com>
2021-12-04 17:07:38 +0800

    Several updates Diff syntax. Fixed source data and result. No more duplication in diffs.

--- src/bin/gen_diff.rs
+++ src/bin/gen_diff.rs
@@ -1,11 +1,11 @@
 use std::{
     fs::{self, File},
     io::{BufWriter, Result, Write},
-    process::Command,
+    process::Command, collections::{HashMap, hash_map::Entry},
 };
 
 fn main() -> Result<()> {
-    let file_stems = file_name_iter().collect::<Vec<_>>();
+    let file_stems = file_stem_iter().collect::<Vec<_>>();
     for pair in file_stems.windows(2) {
         let res = Command::new("git")
             .args(["diff", "-U0", "--no-index"])
@@ -21,20 +21,37 @@ fn main() -> Result<()> {
         }
         let res = unsafe { String::from_utf8_unchecked(res) };
 
-        let file = File::create(format!("diff/{}-{}.diff", pair[0], pair[1]))?;
-        let mut bw = BufWriter::new(file);
+        let mut lines = Vec::<&str>::new();
+        let mut records = HashMap::<&str, usize>::new();
 
         for line in res.lines().skip(4) {
             if !line.starts_with(['@', '\\'].as_ref()) {
+                match records.entry(&line[1..]) {
+                    Entry::Occupied(e) => {
+                        lines[*e.get()] = "";
+                        continue;
+                    }
+                    Entry::Vacant(e) => {
+                        e.insert(lines.len());
+                    }
+                }
+                lines.push(line);
+            }
+        }
+
+        let file = File::create(format!("diff/{}-{}.diff", pair[0], pair[1]))?;
+        let mut bw = BufWriter::new(file);
+       
+        for line in lines {
+            if !line.is_empty() {
                 writeln!(bw, "{}", line)?;
             }
         }
-        bw.flush()?;
     }
     Ok(())
 }
 
-fn file_name_iter() -> impl Iterator<Item = String> {
+fn file_stem_iter() -> impl Iterator<Item = String> {
     fs::read_dir("data").unwrap().map(|e| {
         let path = e.unwrap().path();
         path.file_stem().unwrap().to_string_lossy().into_owned()
