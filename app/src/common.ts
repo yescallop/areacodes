9314de187f55e082486130b1e1127ba340f1c5f9
yescallop <yescallop@gmail.com>
2022-07-02 11:30:31 +0800

    Update

--- app/src/common.ts
+++ app/src/common.ts
@@ -1,57 +1,57 @@
 export interface GlobalProps {
-    options: {
-        hideSuccessors: boolean,
-        hidePredecessors: boolean,
-    },
-    items: Map<number, Item[]>,
-    predecessors: Map<number, Link[]>,
+  options: {
+    hideSuccessors: boolean,
+    hidePredecessors: boolean,
+  },
+  items: Map<number, Item[]>,
+  predecessors: Map<number, Link[]>,
 }
 
 export interface Item {
-    code: number,
-    name: string,
-    start: number,
-    end?: number,
-    successors?: Link[],
-    children?: Item[],
+  code: number,
+  name: string,
+  start: number,
+  end?: number,
+  successors?: Link[],
+  children?: Item[],
 
-    parent?: Item,
-    action?: Action,
-    act?: () => void,
+  parent?: Item,
+  action?: Action,
+  act?: () => void,
 }
 
 export enum Action {
-    // Open only.
-    Open,
-    // Focus only.
-    Focus,
-    // Open if the item is a folder, focus and scroll.
-    OpenFocusScroll,
-    // Close and focus.
-    CloseFocus,
+  // Open only.
+  Open,
+  // Focus only.
+  Focus,
+  // Open if the item is a folder, focus and scroll.
+  OpenFocusScroll,
+  // Close and focus.
+  CloseFocus,
 }
 
 export interface Link {
-    time?: number,
-    code: number,
+  time?: number,
+  code: number,
 }
 
 export interface LinkZipped {
-    codes: number[],
-    time: number,
-    rev: boolean,
+  codes: number[],
+  time: number,
+  rev: boolean,
 }
 
 export function timeOrDefault(link: Link, item: Item): number {
-    return link.time != undefined ? link.time : item.end!;
+  return link.time != undefined ? link.time : item.end!;
 }
 
 export function scrollToItem(item: Item) {
-    item.action = Action.OpenFocusScroll;
-    while (item.act == undefined) {
-        if (item.parent == undefined) return;
-        item = item.parent;
-        item.action = Action.Open;
-    }
-    item.act();
+  item.action = Action.OpenFocusScroll;
+  while (item.act == undefined) {
+    if (item.parent == undefined) return;
+    item = item.parent;
+    item.action = Action.Open;
+  }
+  item.act();
 }
